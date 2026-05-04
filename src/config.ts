2956a28a1219b272f73fd304a9f73d2de3f32dd6
jason195421 <167439185+jason195421@users.noreply.github.com>
2026-05-04 09:11:08 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -369,8 +369,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "まつざか ゆき",
-	bio: "世界は大きい、君は行かなければならない",
+	name: "jasonff",
+	bio: " “人们所说的永远，仅仅是形容当下的炽热”",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
 		speed: 80, // 打字速度（毫秒）
