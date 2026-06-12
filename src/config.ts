61886703b2bd55848af571ab9daad80f07df0de4
jason195421 <167439185+jason195421@users.noreply.github.com>
2026-06-12 16:18:12 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -293,7 +293,7 @@ export const navBarConfig: NavBarConfig = {
 					name: "coolapk",
 					url: "https://gitee.com/matsuzakayuki/Mizuki",
 					external: true,
-					icon: "fa7-brands:coolapk",
+					icon: "simple-icons:coolapk",
 				},
 			],
 		},
