6c1eb5e14a5f12dd39a9be0dfe2b403991d6ed62
jason195421 <167439185+jason195421@users.noreply.github.com>
2026-06-12 16:27:21 +0800

    Change icon source for coolapk in config

--- src/config.ts
+++ src/config.ts
@@ -293,7 +293,7 @@ export const navBarConfig: NavBarConfig = {
 					name: "coolapk",
 					url: "https://gitee.com/matsuzakayuki/Mizuki",
 					external: true,
-					icon: "simple-icons:coolapk",
+					icon: "arcticons:coolapk",
 				},
 			],
 		},
